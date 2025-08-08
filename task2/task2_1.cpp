c200b65666bf43036fcea4740e1c3eb7e9361067
Meshkka <146671780+Meshkka@users.noreply.github.com>
2025-08-08 13:47:46 +0300

    task2

---
+++ task2/task2_1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    std::cout << "Введите число:\n";
+    double value = 0;
+    std::cin >> value;
+    std::cout << "Ваше число:\n" << value;
+}
---
+++ task2/task2_2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::cout << "Введите слово:\n";
+    std::string str = "";
+    std::cin >> str;
+    std::cout << "Ваше слово:\n" << str;
+}
