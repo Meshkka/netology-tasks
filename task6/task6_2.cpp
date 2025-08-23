fef9ab852afbe1f71e1141da976b4eb74903776f
Meshkka <146671780+Meshkka@users.noreply.github.com>
2025-08-23 13:29:50 +0300

    Update task6_2.cpp

--- task6/task6_2.cpp
+++ task6/task6_2.cpp
@@ -1,18 +1,18 @@
 ﻿#include <iostream>
 
-void exp(int value, int power, int result = 1) {
+int exp(int value, int power, int result = 1) {
     for (int i = 0; i < power; i++) {
         result *= value;
     }
 
-    std::cout << value << " в степени " << power << " = " << result << '\n';
+    return result;
 }
 
 int main()
 {
     setlocale(LC_ALL, "RU");
 
-    exp(5, 2);
-    exp(3, 3);
-    exp(4, 4);
+    std::cout << 5 << " в степени " << 2 << " = " << exp(5, 2) << '\n';
+    std::cout << 3 << " в степени " << 3 << " = " << exp(3, 3) << '\n';
+    std::cout << 4 << " в степени " << 4 << " = " << exp(4, 4);
 }
