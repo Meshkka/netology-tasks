7b8f03026742c48cae8943f6026a595e3e7b10ca
Meshkka <146671780+Meshkka@users.noreply.github.com>
2025-08-28 02:29:26 +0300

    task7

---
+++ task7/task7_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    int a = 0;
+    short b = 0;
+    long c = 0;
+    long long d = 0;
+    float e = 0;
+    double f = 0;
+    long double g = 0;
+    bool h = 0;
+
+    std::cout << "int: " << &a << ' ' << sizeof(a) << '\n';
+    std::cout << "short: " << &b << ' ' << sizeof(b) << '\n';
+    std::cout << "long: " << &c << ' ' << sizeof(c) << '\n';
+    std::cout << "long long: " << &d << ' ' << sizeof(d) << '\n';
+    std::cout << "float: " << &e << ' ' << sizeof(e) << '\n';
+    std::cout << "double: " << &f << ' ' << sizeof(f) << '\n';
+    std::cout << "long double: " << &g << ' ' << sizeof(g) << '\n';
+    std::cout << "bool: " << &h << ' ' << sizeof(h);
+}
---
+++ task7/task7_2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void swap(int& a, int& b) {
+	if (a == b) { return; }
+	a ^= b;
+	b ^= a;
+	a ^= b;
+}
+
+int main(int argc, char** argv)
+{
+	int a = 5, b = 8;
+
+	std::cout << "a = " << a << ", b = " << b << '\n';
+
+	swap(a, b);
+
+	std::cout << "a = " << a << ", b = " << b;
+
+	return 0;
+}
