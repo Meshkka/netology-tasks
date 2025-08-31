340d64bc534a932f0a154a49170195afa6bbaa22
Meshkka <146671780+Meshkka@users.noreply.github.com>
2025-09-01 01:54:49 +0300

    task8

---
+++ task8/task8_1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+void print(int value) {
+    std::cout << "Количество вызовов функции counting_function(): " << value << '\n';
+}
+
+void counting_function() {
+    static int counter = 0;
+    counter++;
+
+    print(counter);
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "RU");
+
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function();
+    }
+}
---
+++ task8/task8_2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+namespace addition 
+{
+    int calc(int x, int y) {
+        return x + y;
+    }
+}
+
+namespace subtraction
+{
+    int calc(int x, int y) {
+        return x - y;
+    }
+}
+
+namespace multiplication
+{
+    int calc(int x, int y) {
+        return x * y;
+    }
+}
+
+namespace division
+{
+    int calc(int x, int y) {
+        return x / y;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int x = 6, y = 9;
+
+    std::cout << "Сложение: " << addition::calc(x, y) << '\n';
+    std::cout << "Вычитание: " << subtraction::calc(x, y) << '\n';
+    std::cout << "Умножение: " << multiplication::calc(x, y) << '\n';
+    std::cout << "Деление: " << division::calc(x, y);
+}
