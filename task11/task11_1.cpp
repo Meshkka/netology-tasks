6c3363663f89ee8d20b54d8ef736d91dc5e3f2a8
Meshkka <146671780+Meshkka@users.noreply.github.com>
2025-09-12 15:07:37 +0300

    task11

---
+++ task11/task11_1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string name = "", surname = "";
+    std::cout << "Введите имя: ";
+    std::cin >> name;
+    std::cout << "Введите фамилию: ";
+    std::cin >> surname;
+
+    std::cout << "Здравствуйте, " + name + ' ' + surname + '!';
+}
---
+++ task11/task11_2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::cout << "Введите слово, которое надо угадать: ";
+    std::string word1 = "", word2 = "";
+    std::cin >> word1;
+
+    bool flag = true;
+    while (flag) {
+        std::cout << "Угадайте слово: ";
+        std::cin >> word2;
+        
+        if (word1 == word2) {
+            std::cout << "Правильно! Вы победили! Загаданное слово — " + word1;
+            flag = false;
+        }
+        else {
+            std::cout << "Неправильно\n";
+        }
+    }
+}
